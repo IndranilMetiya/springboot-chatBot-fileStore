5a77cc750ceac21f99bf2fffdfe80229f24cbe05
IndranilMetiya <47168386+IndranilMetiya@users.noreply.github.com>
2024-06-29 14:57:03 +0530

    Update AppConstants.java

--- src/main/java/com/chatBot/indraChat/constants/AppConstants.java
+++ src/main/java/com/chatBot/indraChat/constants/AppConstants.java
@@ -3,7 +3,7 @@ package com.chatBot.indraChat.constants;
 public class AppConstants {
 	
 	public static final String API_URL="https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=%s";
-	public static final String GEMINI_KEY="AIzaSyApswSX0L9bkI4W1QFbbWoG7FFhprOQfco";
+	public static final String GEMINI_KEY="Give Your Secret API Key here from https://ai.google.dev/aistudio";
 	public static final String LOG_FILE_PATH = "C:/Users/User/OneDrive/Documents/ChatBotLogs/";
 	
 
